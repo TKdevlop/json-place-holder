abca8759d894b4062b2c557e16a041be62b30a0c
TUSHAR KHARBANDA <tusharkharbanda13@gmail.com>
2018-11-20 17:05:45 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Addposts from "./components/Addposts";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteForever from "@material-ui/icons/DeleteForever";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const useFetch = async (input, settings = {}) => {
+  const response = await fetch(input, {
+    headers: {
+      Accept: "application/json"
+    },
+    ...settings
+  });
+  return response.json();
+};
+const addPosts = async (title, body, posts, setPosts) => {
+  console.log("ADDPOSTS");
+  const response = await useFetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
+      method: "POST",
+      contentType: "application/json",
+      body: JSON.stringify({ title, body })
+    }
+  );
+  setPosts([...posts, { title, body, id: Math.random() * 1000 }]);
+};
+
+const getSinglePost = async id => {
+  const res = await useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  console.log(res);
+};
+const deleteSinglePost = async (posts, post, setPosts) => {
+  const res = await useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${post.id}`,
+    {
+      method: "DELETE"
+    }
+  );
+  if (!res.keys) {
+    const updatedPosts = posts.filter(thisPost => thisPost.id !== post.id);
+    setPosts([...updatedPosts]);
   }
-}
+};
 
-export default App;
+const renderPosts = (posts, setPosts) =>
+  posts.map(post => (
+    <ListItem key={post.id} dense button onClick={() => {}}>
+      <ListItemText
+        onClick={() => getSinglePost(post.id)}
+        style={{
+          fontSize: 16
+        }}
+        primary={post.body}
+      />
+
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete">
+          <DeleteForever
+            onClick={() => {
+              deleteSinglePost(posts, post, setPosts);
+            }}
+          />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  ));
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    const posts = await useFetch("https://jsonplaceholder.typicode.com/posts");
+    setPosts([...posts]);
+  }, []);
+
+  return (
+    <div>
+      <Addposts
+        onSubmit={(title, body) => addPosts(title, body, posts, setPosts)}
+      />
+
+      <List component="ul">{renderPosts(posts, setPosts)}</List>
+    </div>
+  );
+}
---
+++ src/components/Addposts/Addposts.js
@@ -0,0 +1,70 @@
+import React, { useState, useImperativeMethods } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useInputValue = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    onChange: e => setValue(e.target.value),
+    resetvalue: () => setValue(""),
+    updatedvalue: (todo, todos, setTodos) => {
+      setValue(todo.value);
+      const updatedTodos = todos.filter(
+        localTodo => localTodo.value !== todo.value
+      );
+      setTodos([...updatedTodos]);
+    }
+  };
+};
+export default function Addposts({ onSubmit }, ref) {
+  const title = useInputValue("");
+  const body = useInputValue("");
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(title.value, body.value);
+        title.resetvalue();
+        body.resetvalue();
+      }}
+      style={{ maxWidth: "60%", margin: "0 auto", padding: 30 }}
+    >
+      <TextField
+        {...title}
+        id="standard-full-width"
+        label="Title"
+        style={{ margin: 8 }}
+        placeholder="Enter Post Title"
+        helperText="post tille"
+        fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true
+        }}
+      />
+      <TextField
+        {...body}
+        id="standard-full-width"
+        label="Body"
+        style={{ margin: 8 }}
+        placeholder="Enter Post body"
+        helperText="post body"
+        fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true
+        }}
+      />
+      <Button
+        type="submit"
+        fullWidth
+        style={{ fontSize: 24, textAlign: "center" }}
+        color="primary"
+      >
+        ADD POST
+      </Button>
+    </form>
+  );
+}
